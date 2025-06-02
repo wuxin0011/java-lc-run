57b4d38d8a6e2f1606fe9e8a3918ab45f5ce6ca7
wuxin0011 <2191377759@qq.com>
2025-06-02 18:03:48 +0800

    fix:bug new version

---
+++ src/test/java/day/everyday.java
@@ -0,0 +1,9 @@
+package day;
+
+import code_generation.crwal.leetcode.LCEveryDay;
+
+public class everyday {
+    public static void main(String[] args) {
+        LCEveryDay.everyDay.start(everyday.class);
+    }
+}
